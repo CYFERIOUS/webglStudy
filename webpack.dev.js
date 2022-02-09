710e7cf9a3e31d9bb08068837b3bf124c3fc0298
CYFERIOUS <frankdrujes@gmail.com>
2022-02-09 13:18:56 -0500

    last push for study

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,8 +2,6 @@ const path = require("path");
 const common = require("./webpack.common");
 const {merge} = require("webpack-merge");
 
-
-
 module.exports = merge(common,{
   mode: "development",
   output: {
